7d8d68eb6d36790e0d7e899e1ae0f3620eb0d011
nagarizki <akuakusaya@gmail.com>
2026-03-30 11:35:09 +0700

    chore: fix routing for the blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,7 +48,7 @@ const fontsConfig = Object.entries(theme.fonts.font_family)
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "https://nagarizki.github.io",
+  site: config.site.base_url ? config.site.base_url + config.site.base_path : "https://nagarizki.github.io/site",
   base: config.site.base_path ? config.site.base_path : "/site",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: { service: sharp() },
